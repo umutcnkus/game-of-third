dc0b077ee460bb82a252d278a31bd2004409b55c
Umutcan Kuş <umutcan.kus@argela.com.tr>
2020-11-16 21:02:02 +0300

    Style changes

--- src/Possibilities/DivideByThreePossibility.ts
+++ src/Possibilities/DivideByThreePossibility.ts
@@ -1,7 +1,7 @@
 import { GameStrategy, Possibility } from "../interfaces";
 
 export class DivideByPossibility implements Possibility {
-    isPossible = (current: number, value: number, gameStrategy: GameStrategy) =>Number.isInteger(gameStrategy.play({
+    isPossible = (current: number, value: number, gameStrategy: GameStrategy) => Number.isInteger(gameStrategy.play({
         before: current,
         played: value
     }).after);
